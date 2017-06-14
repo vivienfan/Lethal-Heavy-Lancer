f3ee7910837027efc731d21e3079274433a9de62
Armel Chesnais <armel.chesnais@gmail.com>
2017-06-14 18:52:16 +0000

    initial Websocket connection

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 "use strict";
 
-const PORT            = 8080;
-// const MONGODB_URI     = "mongodb://localhost:27017/tweeter";
-const HASHROUNDS      = 10;
-const express         = require("express");
-// const bodyParser      = require("body-parser");
-const app             = express();
-// const cookieSession   = require("cookie-session");
-// const MongoClient     = require("mongodb").MongoClient;
-// const sassMiddleware  = require('node-sass-middleware');
-// const methodOverride  = require('method-override')
-const bcrypt          = require("bcrypt");
+const PORT          = 8080;
+const express       = require("express");
+const WebSocket     = require('ws');
+const SocketServer  = WebSocket.Server;
+const uuidV4        = require('uuid/v4');
+const app           = express();
+const bcrypt        = require("bcrypt");
 
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use(methodOverride("_method"));
 
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// app.use(cookieSession({
-//   name: 'session',
-//   keys: ['SuperTotallySecretKey1', 'AnotherReallySecretKey2'], // Secret keys
+const wss = new SocketServer({ server });
 
-//   // Cookie options
-//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-// }));
+wss.on('connection', (ws) => {
+  console.log('Client connected')
+  let count = 0
 
-// app.use(sassMiddleware({
-//   src: './server/stylesheets',
-//   dest: './public/styles',
-//   prefix: '/styles'
-// }));
+  let timer = setInterval(function() {
+    count %= 5;
+    count++;
+    ws.send(JSON.stringify({'count': count}));
+    console.log('sending count:', count)
+  }, 2000);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  ws.on('message', function incoming(message) {
+    message = JSON.parse(message)
+    // message.id = uuidV4()
+
+    wss.clients.forEach( (c) => {
+      if (c.readyState === WebSocket.OPEN) {
+
+        c.send(JSON.stringify(message));
+      }
+    })
+
+  });
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    // userCount--
+    clearInterval(timer)
+  });
 });
