c163eef90e2a85cb98f1d506bb9c8e1431bdf79e
Dong Hu <cdhu@uwaterloo.ca>
2017-06-16 02:40:27 +0000

    Render ground and skybox

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,6 +51,15 @@ window.onload = function() {
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     skybox.material = skyboxMaterial;
 
+    // Create ground
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+    extraGroundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+    extraGroundMaterial.diffuseTexture.uScale = 60;
+    extraGroundMaterial.diffuseTexture.vScale = 60;
+    extraGround.position.y = -2.05;
+    extraGround.material = extraGroundMaterial;
+
     BABYLON.SceneLoader.ImportMesh("", "", "walk.babylon", scene, function (newMeshes, particleSystems, skeletons) {
       player = newMeshes;
       player[0].rotation.y = Math.PI;
