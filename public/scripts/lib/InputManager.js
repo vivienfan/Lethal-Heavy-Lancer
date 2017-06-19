b760cbc1bbad6392231683b6de981d900fca615b
Armel Chesnais <armel.chesnais@gmail.com>
2017-06-19 18:36:01 +0000

    separating file for input manager

---
+++ public/scripts/lib/InputManager.js
@@ -0,0 +1,98 @@
+// inputManager.js
+
+var ANGLE = Math.PI/180;
+var SPEED = 2;
+
+function InputManager(player) {
+    console.log("input player", player)
+    this.isPressed = {}
+    this.lastY = 0
+
+    this.process = function(type, event) {
+      console.log("process player", player)
+      // we want to update mousemove directly, as it is a direct relation to how far user moved mouse
+      if ( type === "mousemove" ) {
+        // player.rotationY += event.movementX * ANGLE
+        // player.rotationX += event.movementY * ANGLE
+      } else {
+        // otherwise, it is a key input. From here, determine the key, modify the relevant speed, and
+        // apply, so it can be used on the next update call. Allows smooth movement independent of framerate
+        // and input frequency
+        switch ( event.key ) {
+          case "w":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.fwdSpeed = SPEED
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.fwdSpeed = 0
+            }
+            break;
+          case "s":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.fwdSpeed = -(SPEED)
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.fwdSpeed = 0
+            }
+            break;
+          case "a":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.sideSpeed = SPEED
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.sideSpeed = 0
+            }
+            break;
+          case "d":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.sideSpeed = -SPEED
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.sideSpeed = 0
+            }
+            break;
+          case "ArrowRight":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.rotYSpeed = ANGLE
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.rotYSpeed = 0
+            }
+            break;
+          case "ArrowLeft":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.rotYSpeed = -(ANGLE)
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.rotYSpeed = 0
+            }
+            break;
+          case "ArrowUp":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.rotXSpeed = -(ANGLE)
+              // console.log("Set rotspeed")
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.rotXSpeed = 0
+            }
+            break;
+          case "ArrowDown":
+            if ( type === "keydown" && !this.isPressed[event.key] ) {
+              this.isPressed[event.key] = true
+              player.rotXSpeed = ANGLE
+            } else if ( type === "keyup" ){
+              this.isPressed[event.key] = false
+              player.rotXSpeed = 0
+            }
+            break;
+          } // end of switch statement
+        }
+      } // end of process method
+  } // end of InputManager class
