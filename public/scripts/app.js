e855e748f28256e4a63c6f69866716be8b917099
vagrant <vivien.fan.1224@gmail.com>
2017-06-26 18:32:29 +0000

    moved scene render update and socket msg send

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -534,63 +534,7 @@ window.onload = function() {
     }
   }
 
-  function updatePlayerOrientation() {
-    playerStatus.rotation.y += player.rotationY;
-    player.rotationY = 0;
-    playerStatus.rotation.y += player.rotYSpeed * scene.getAnimationRatio();
-    playerStatus.rotation.y = playerStatus.rotation.y % (2 * Math.PI);
-    avatar.rotation.y = playerStatus.rotation.y;
-    cameraTarget.rotation.y = playerStatus.rotation.y;
-    camera.alpha = - (playerStatus.rotation.y + ALPHA_OFFSET);
 
-    // rotation on x-axis
-    var tmp_angle = camera.beta - player.rotationX;
-    tmp_angle = camera.beta - player.rotXSpeed * scene.getAnimationRatio();
-    player.rotationX = 0;
-    camera.beta = Math.min(Math.max(tmp_angle, DOWN_ANGLE_MAX), UP_ANGLE_MAX);
-    avatar.rotation.x = camera.beta - BETA_OFFSET;
-    cameraTarget.rotation.x = avatar.rotation.x + CAMERA_TARGET_OFFSET;
-
-    // move forward/backward
-    playerStatus.position.x += player.fwdSpeed * Math.sin(playerStatus.rotation.y + Math.PI) * scene.getAnimationRatio();
-    playerStatus.position.z += player.fwdSpeed * Math.cos(playerStatus.rotation.y + Math.PI) * scene.getAnimationRatio();
-
-    // move left/right
-    playerStatus.position.x += player.sideSpeed * -Math.cos(playerStatus.rotation.y + Math.PI) * scene.getAnimationRatio();
-    playerStatus.position.z += player.sideSpeed * Math.sin(playerStatus.rotation.y + Math.PI) * scene.getAnimationRatio();
-
-    // collision handling
-    var direction = new BABYLON.Vector3(
-      playerStatus.position.x - avatar.position.x, 0,
-      playerStatus.position.z - avatar.position.z);
-    avatar.moveWithCollisions(direction);
-
-    // collision engine auto-adjust position when collision happens
-    // avatar.position.y = 0;
-    cameraTarget.position.x = avatar.position.x;
-    cameraTarget.position.y = avatar.position.y + CAM_OFFSET;
-    cameraTarget.position.z = avatar.position.z;
-    playerStatus.position.x = avatar.position.x;
-    playerStatus.position.y = avatar.position.y;
-    playerStatus.position.z = avatar.position.z;
-  }
-
-  function sendPlayerState() {
-    if( playerStatus && socket.readyState === socket.OPEN ) {
-      var msg = {
-        type: CONSTANTS.MESSAGE_TYPE.UPDATE,
-        player: Object.assign({},player)
-      };
-      msg.player.id = playerStatus.id;
-      msg.player.rotation = avatar.rotation;
-      msg.player.position = avatar.position;
-      msg.player.fwdSpeed = player.fwdSpeed;
-      msg.player.sideSpeed = player.sideSpeed;
-
-      // Send the msg object as a JSON-formatted string.
-      socket.send(JSON.stringify(msg));
-    }
-  }
 
   function updateCharacterOriendtation() {
     characterStatus.forEach(function(character) {
